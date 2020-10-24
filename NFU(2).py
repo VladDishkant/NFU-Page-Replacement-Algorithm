2e1c02c3b79c51ad537dea8ab4939fdf3261a0c5
VladDishkant <vlad.dishkant111@gmail.com>
2020-10-24 18:27:42 +0300

    Add NFU(2)

---
+++ NFU(2).py
@@ -0,0 +1,15 @@
+
+
+def simulate_aging(Rs, k: int) -> None:
+    """Simulate aging."""
+    Vs = [0] * len(Rs[0])
+    for t, R in enumerate(Rs):
+        for i in range(len(Vs)):
+            Vs[i] = R[i] << k - 1 | Vs[i] >> 1
+        if t == 0:
+            print('Tact|           R          |      %s' % '         '.join([str(i) for i in range(len(R))]))
+        print('%02d  |  %s  |  [%s]' % (t, R, ', '.join([format(V, '0%db' % k) for V in Vs])))
+
+Rs = [[1,0,1,0,1,1], [1,1,0,0,1,0], [1,1,0,1,0,1], [1,0,0,0,1,0], [0,1,1,0,0,0]]
+k = 8
+simulate_aging(Rs, k)
